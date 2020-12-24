7753433eb4327f9e5d1441d61584f7e2a9e6f94c
Christian <summersedge23@gmail.com>
2020-12-24 09:27:08 -0500

    Cleaning up ui.
    Signed-off-by: Christian <summersedge23@gmail.com>

--- src/elements/XNORGate.ts
+++ src/elements/XNORGate.ts
@@ -842,6 +842,7 @@ or overlapped)*/
 				if (
 					global.WIRE_BUILDER['step'] === 0 &&
 					this.bounds.contains_xywh(global.mouse_x, global.mouse_y, this.bounds.get_width() * 1.25, this.bounds.get_height() * 1.25) &&
+					!multi_select_manager.MULTI_SELECT &&
 					!this.MULTI_SELECTED &&
 					!global.FLAG_SAVE_CIRCUIT &&
 					!global.FLAG_SAVE_IMAGE &&
