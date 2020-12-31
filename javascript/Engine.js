b2499179069621c0a449b1b299c9c681a99db347
Christian <summersedge23@gmail.com>
2020-12-31 08:30:56 -0500

    Found a bug in resetting all events :')
    Signed-off-by: Christian <summersedge23@gmail.com>

--- javascript/Engine.js
+++ javascript/Engine.js
@@ -815,22 +815,6 @@ function load_app() {
                                 if (global.canvas_draw_request_counter++ >= global.CANVAS_DRAW_REQUEST_COUNTER_MAX) {
                                     global.canvas_draw_request_counter = 0;
                                     global.canvas_draw_request = false;
-                                    /* Reset all Events. */
-                                    global.mouse_down_event_queue.splice(0, global.mouse_down_event_queue.length);
-                                    global.mouse_up_event_queue.splice(0, global.mouse_up_event_queue.length);
-                                    global.mouse_double_click_event_queue.splice(0, global.mouse_double_click_event_queue.length);
-                                    global.mouse_wheel_event_queue.splice(0, global.mouse_wheel_event_queue.length);
-                                    global.key_down_event_queue.splice(0, global.key_down_event_queue.length);
-                                    global.key_up_event_queue.splice(0, global.key_up_event_queue.length);
-                                    MOUSE_EVENT_LATCH = false;
-                                    global.MOUSE_DOWN_EVENT = false;
-                                    global.MOUSE_MOVE_EVENT = false;
-                                    global.MOUSE_UP_EVENT = false;
-                                    global.MOUSE_DOUBLE_CLICK_EVENT = false;
-                                    global.MOUSE_WHEEL_EVENT = false;
-                                    global.RESIZE_EVENT = false;
-                                    global.KEY_DOWN_EVENT = false;
-                                    global.KEY_UP_EVENT = false;
                                 }
                             }
                         }
@@ -912,6 +896,7 @@ function load_app() {
                 if (global.mouse_up_event_queue.length === 0) {
                     global.MOUSE_UP_EVENT = false;
                     global.MOUSE_MOVE_EVENT = false;
+                    global.is_dragging = false;
                 }
                 global.canvas_draw_request = true;
                 global.canvas_draw_request_counter = 0;
@@ -937,6 +922,7 @@ function load_app() {
                 }
                 global.canvas_draw_request = true;
                 global.canvas_draw_request_counter = 0;
+                global.is_dragging = false;
             }
             if (global.RESIZE_EVENT) {
                 general_paint.set_stroke_width(global.CANVAS_STROKE_WIDTH_1);
